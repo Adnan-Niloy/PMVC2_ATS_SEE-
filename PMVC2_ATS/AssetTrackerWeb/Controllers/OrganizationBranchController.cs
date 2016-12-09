4c632dde5a5046f8043a9c3869c550928dfdafe0
adnanniloy1313@gmail.com <adnanniloy1313@gmail.com>
2016-12-10 04:19:57 +0600

    Organization controller to add organization with the simplest form in view

---
+++ PMVC2_ATS/AssetTrackerWeb/Controllers/OrganizationBranchController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AssetTrackerWeb.Controllers
+{
+    public class OrganizationBranchController : Controller
+    {
+        // GET: OrganizationBranch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PMVC2_ATS/AssetTrackerWeb/Controllers/OrganizationController.cs
@@ -0,0 +1,31 @@
+﻿using AssetTracker.Core.BLL;
+using AssetTracker.Core.Models;
+using System.Web.Mvc;
+
+namespace AssetTrackerWeb.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        private readonly OrganizationManager _manager = new OrganizationManager();
+
+        public ActionResult Entry()
+        {
+            var organization = new Organization();
+            return View(organization);
+        }
+
+        [HttpPost]
+        public ActionResult Entry(Organization organization)
+        {
+            var isAdded = _manager.Add(organization);
+
+            if (isAdded)
+                ViewBag.SuccessMessage = "Organization Created Successfully";
+
+            ViewBag.ErrorMessage = "Organization Creation Failed";
+
+            return RedirectToAction("Entry", "Organization");
+        }
+
+    }
+}
