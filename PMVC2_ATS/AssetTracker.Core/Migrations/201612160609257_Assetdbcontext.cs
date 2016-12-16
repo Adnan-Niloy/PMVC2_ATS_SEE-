8ebe27fc632219bacebba6abe5800400c73a0143
SumitraBarua <barua_sumitra@yahoo.com>
2016-12-16 23:49:33 +0600

    add template with layout and view.cshtml

---
+++ PMVC2_ATS/AssetTracker.Core/Migrations/201612160609257_Assetdbcontext.cs
@@ -0,0 +1,90 @@
+namespace AssetTracker.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Assetdbcontext : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.String(),
+                        Description = c.String(),
+                        GeneralCategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.GeneralCategories", t => t.GeneralCategoryId, cascadeDelete: true)
+                .Index(t => t.GeneralCategoryId);
+            
+            CreateTable(
+                "dbo.GeneralCategories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.OrganizationBranches",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ShortName = c.String(),
+                        OrganizationId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Organizations", t => t.OrganizationId, cascadeDelete: true)
+                .Index(t => t.OrganizationId);
+            
+            CreateTable(
+                "dbo.Organizations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.String(),
+                        Location = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SubCategories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.String(),
+                        Description = c.String(),
+                        GeneralCategoryId = c.Int(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SubCategories", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.OrganizationBranches", "OrganizationId", "dbo.Organizations");
+            DropForeignKey("dbo.Categories", "GeneralCategoryId", "dbo.GeneralCategories");
+            DropIndex("dbo.SubCategories", new[] { "CategoryId" });
+            DropIndex("dbo.OrganizationBranches", new[] { "OrganizationId" });
+            DropIndex("dbo.Categories", new[] { "GeneralCategoryId" });
+            DropTable("dbo.SubCategories");
+            DropTable("dbo.Organizations");
+            DropTable("dbo.OrganizationBranches");
+            DropTable("dbo.GeneralCategories");
+            DropTable("dbo.Categories");
+        }
+    }
+}
